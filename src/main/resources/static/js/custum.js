e421e3c8c1a2e0fd1a084e925c8401186c3d0857
gabida <gabida.steam@gmail.com>
2020-07-11 14:08:04 +0200

    index.html modifications add card read more function  + removing nav and letgo became login

--- src/main/resources/static/js/custum.js
+++ src/main/resources/static/js/custum.js
@@ -1,3 +1,24 @@
+ function myFunction(elment) {
+    
+var spans =  elment.parentNode.querySelectorAll("span")
+ 	console.log(elment)
+ 	console.log(elment.parentNode)
+ 	console.log(elment.parentNode.querySelectorAll("span"))
+ 	
+  var dots = spans[0]
+  var moreText = spans[1]
+  var btnText = elment;
+
+  if (dots.style.display === "none") {
+    dots.style.display = "inline";
+    btnText.innerHTML = "Read more";
+    moreText.style.display = "none";
+  } else {
+    dots.style.display = "none";
+    btnText.innerHTML = "Read less";
+    moreText.style.display = "inline";
+  }
+ }
 $(() => {
     let carouselImages = $("#carouselExampleIndicators div div");
 
